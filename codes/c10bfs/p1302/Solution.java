a5757c7dcec75c09ebd4fa7e301878f8dd9fbb62
JasonNerd <duan912rainbow@qq.com>
2024-03-19 19:58:25 +0800

    breadth first search

--- codes/c10bfs/p1302/Solution.java
+++ codes/c10bfs/p1302/Solution.java
@@ -1,10 +1,28 @@
 package codes.c10bfs.p1302;
 
-import codes.c02string.p2096.TreeNode;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
+import codes.c02string.p2096.TreeNode;
+// https://zhuanlan.zhihu.com/p/136183284
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        int cum = 0;
+        while (!q.isEmpty()) {
+            int n = q.size();
+            cum = 0;    // 当前层节点值的和
+            // 每次都是对某一层的节点统一处理(而不是单个单个的处理)
+            for (int i=0; i<n; i++){
+                TreeNode node = q.poll();
+                cum += node.val;
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
 
+        }
+        return cum;
     }
 }
 /*
