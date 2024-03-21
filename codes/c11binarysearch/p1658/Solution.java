675e3a3b04223e35f10c7d16a8eabd43e45932b8
JasonNerd <duan912rainbow@qq.com>
2024-03-21 23:59:33 +0800

    滑动窗口解题

--- codes/c11binarysearch/p1658/Solution.java
+++ codes/c11binarysearch/p1658/Solution.java
@@ -3,35 +3,60 @@ package codes.c11binarysearch.p1658;
 // https://books.halfrost.com/leetcode/ChapterFour/1600~1699/1658.Minimum-Operations-to-Reduce-X-to-Zero/
 
 public class Solution {
-
+    public int minOperations(int[] nums, int x) {
+        int total = 0, n = nums.length;
+        for (int i=0; i<n; i++)
+            total += nums[i];
+        if (total == x) return 0;
+        if (total < x) return -1;
+        int l=0, r=0, res=-1, target=total-x, sum=0;
+        while (r < n){
+            if (sum < target)
+                sum += nums[r++];
+            while (sum >= target) {
+                if (sum == target){
+                    res = res > (r-l) ? res : (r-l);
+                }
+                sum -= nums[l++];
+            }
+        }
+        return res>0?n - res:res;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {5,2,3,1,1};
+        int x = 5;
+        System.out.println(new Solution().minOperations(nums, x));
+    }
 }
 /*
 1658. 将 x 减到 0 的最小操作数
-给你一个整数数组 nums 和一个整数 x 。每一次操作时，你应当移除数组 nums 最左边或最右边的元素，然后从 x 中减去该元素的值。请注意，需要 修改 数组以供接下来的操作使用。
-
-如果可以将 x 恰好 减到 0 ，返回 最小操作数 ；否则，返回 -1 。
-
- 
+给你一个整数数组 nums 和一个整数 x 。每一次操作时，你应当移除数组 nums 最左边或最右边的元素，然后从 x 中减去该元素的值。
+请注意，需要 修改 数组以供接下来的操作使用。如果可以将 x 恰好 减到 0 ，返回 最小操作数 ；否则，返回 -1 。
 
 示例 1：
-
 输入：nums = [1,1,4,2,3], x = 5
 输出：2
 解释：最佳解决方案是移除后两个元素，将 x 减到 0 。
-示例 2：
 
+示例 2：
 输入：nums = [5,6,7,8,9], x = 4
 输出：-1
-示例 3：
 
+示例 3：
 输入：nums = [3,2,20,1,1,3], x = 10
 输出：5
 解释：最佳解决方案是移除后三个元素和前两个元素（总共 5 次操作），将 x 减到 0 。
  
 
 提示：
-
 1 <= nums.length <= 105
 1 <= nums[i] <= 104
 1 <= x <= 109
- */
+*/
+/*
+先把问题转化一下, 问题是要从数组两头去掉尽可能少的数字, 使得被去掉的数字之和恰好为 x.
+等价于考虑这样一个窗口, 窗口内的数字之和为 sum(nums)-x, 同时窗口最大.
+
+如何维护窗口呢, 宽度从0开始, 一直扩大直到超出范围, 随后缩小直到在范围内(根据条件判断是要缩小还是扩张)
+*/
--- codes/c11binarysearch/p1818/Solution.java
+++ codes/c11binarysearch/p1818/Solution.java
@@ -2,39 +2,36 @@ package codes.c11binarysearch.p1818;
 // https://books.halfrost.com/leetcode/ChapterFour/1800~1899/1818.Minimum-Absolute-Sum-Difference/
 // https://leetcode.cn/problems/minimum-absolute-sum-difference/description/
 public class Solution {
+    public static final int MOD = 1000000007;
 
+    public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
+        // 1. 必然是替换差值最大的一组(如果需要的话)
+    }
 }
 /*
 1818. 绝对差值和
 给你两个正整数数组 nums1 和 nums2 ，数组的长度都是 n 。
-
 数组 nums1 和 nums2 的 绝对差值和 定义为所有 |nums1[i] - nums2[i]|（0 <= i < n）的 总和（下标从 0 开始）。
-
 你可以选用 nums1 中的 任意一个 元素来替换 nums1 中的 至多 一个元素，以 最小化 绝对差值和。
-
 在替换数组 nums1 中最多一个元素 之后 ，返回最小绝对差值和。因为答案可能很大，所以需要对 109 + 7 取余 后返回。
-
 |x| 定义为：
-
 如果 x >= 0 ，值为 x ，或者
 如果 x <= 0 ，值为 -x
  
-
 示例 1：
-
 输入：nums1 = [1,7,5], nums2 = [2,3,5]
 输出：3
 解释：有两种可能的最优方案：
 - 将第二个元素替换为第一个元素：[1,7,5] => [1,1,5] ，或者
 - 将第二个元素替换为第三个元素：[1,7,5] => [1,5,5]
 两种方案的绝对差值和都是 |1-2| + (|1-3| 或者 |5-3|) + |5-5| = 3
-示例 2：
 
+示例 2：
 输入：nums1 = [2,4,6,8,10], nums2 = [2,4,6,8,10]
 输出：0
 解释：nums1 和 nums2 相等，所以不用替换元素。绝对差值和为 0
-示例 3：
 
+示例 3：
 输入：nums1 = [1,10,4,4,2,7], nums2 = [9,3,5,1,7,4]
 输出：20
 解释：将第一个元素替换为第二个元素：[1,10,4,4,2,7] => [10,10,4,4,2,7]
@@ -42,7 +39,6 @@ public class Solution {
  
 
 提示：
-
 n == nums1.length
 n == nums2.length
 1 <= n <= 105
